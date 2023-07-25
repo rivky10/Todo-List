21d3f1fd00628738befb9602644ff0b0bff98ab3
Sagi <rivkygoren@gmail.com>
2023-07-25 17:12:03 +0300

    update back

---
+++ Todo-List-Backend/src/gql/resolversTodo.ts
@@ -0,0 +1,49 @@
+import { MyDataSource } from '../app';
+import { Task } from '../entity/todo';
+
+export const resolversTodo = {
+  Query: {
+    tasks: async () => {
+      const tasksRepository = MyDataSource.getRepository(Task);
+      const tasks = await tasksRepository.find({
+        order: {
+          id: 'ASC',
+        },
+      });
+      return tasks;
+    },
+  },
+
+  Mutation: {
+    addTask: async (_: any, { name }: { name: string }) => {
+      const tasksRepository = MyDataSource.getRepository(Task);
+      const task = tasksRepository.create({ name, iscompleted: false });
+      const newTask = await MyDataSource.getRepository(Task).save(task);
+      return newTask;
+    },
+
+    deleteTask: async (_: any, { id }: { id: number }) => {
+      const tasksRepository = MyDataSource.getRepository(Task);
+      const taskToDelete = await tasksRepository.findOne({ where: { id } });
+
+      if (!taskToDelete) {
+        return false; // Task with the given ID was not found
+      }
+
+      await tasksRepository.delete(id);
+      return true; // Task successfully deleted
+    },
+
+    toggleTask: async (_: any, { id }: { id: number }) => {
+      const tasksRepository = MyDataSource.getRepository(Task);
+      const task = await tasksRepository.findOne({ where: { id } });
+      if (!task) {
+        throw new Error('Task not found');
+      }
+
+      task.iscompleted = !task.iscompleted;
+      await tasksRepository.save(task);
+      return task;
+    },
+  },
+};
--- my-app-front/src/App.tsx
+++ my-app-front/src/App.tsx
@@ -1,14 +1,7 @@
-import { ApolloProvider } from '@apollo/client';
 import './App.css';
-import TodoList from './components/TodoList';
-import { apolloClient } from './services/apiService';
 
 const App: React.FC = () => {
-  return (
-    <ApolloProvider client={apolloClient}>
-      <TodoList />
-    </ApolloProvider>
-  );
+  return <p>h</p>;
 };
 
 export default App;
